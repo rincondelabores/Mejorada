8935d3f78a256e2159af6e1ddeaf8a9967db5016
rincondelabores <rincondelabores@gmail.com>
2025-11-14 14:39:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -412,7 +412,7 @@ function calcularPatron() {
         // 5. Holgura de Sisa (MODIFICADO: Nueva lógica 2/3/4 cm)
         let holguraAxilaCm; 
         if (ORDEN_TALLAS['Bebé (Prematuro a 24m)'].includes(tallaSeleccionada)) {
-            holguraAxilaCm =  mmedidas.AE* 0.20; //2.0; // 2c
+            holguraAxilaCm =  2.0; // 2c
         } else if (ORDEN_TALLAS['Niños (3 a 10 años)'].includes(tallaSeleccionada)) {
             holguraAxilaCm = 4.0; // 4 cm
         } else if (ORDEN_TALLAS['Adulto (36 a 50)'].includes(tallaSeleccionada)) {
